855b7dbd4f0336a0649beb6b6e41102ea220d5cb
kangping115 <59699749+kangping115@users.noreply.github.com>
2020-04-22 13:06:37 -0700

    added converting the keypoint to coordinates

--- kp/main.py
+++ kp/main.py
@@ -38,9 +38,9 @@ if __name__ == '__main__':
     img2_dir = '../data/panorama-data1/DSC01540.JPG'
 
     img1 = io.imread(img1_dir)
-    img2 = io.imread(img1_dir)
+    img2 = io.imread(img2_dir)
 
-    extract_func = "ORB"
+    extract_func = "SIFT"
 
     # Extract key points and descriptors
     # Three types of feature extraction
@@ -50,21 +50,10 @@ if __name__ == '__main__':
     kp1, des1 = ExtractFeatures(img1, extract_func, num_features)
     kp2, des2 = ExtractFeatures(img2, extract_func, num_features)
 
-    print(img1.shape)
-    print(des1.shape)
+    # convert the keypoints to coordinates
+    xy_kp1 = cv2.KeyPoint_convert(kp1)
+    xy_kp2 = cv2.KeyPoint_convert(kp2)
 
-    # Match descriptors
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
-
-    # Sort them in the order of their distance.
-    matches = sorted(matches, key=lambda x: x.distance)
-
-    # Draw all matches.
-    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=2)
-
-    # Display results
-    fig = plt.figure()
-    plt.imshow(img3)
-    plt.axis('off')
-    plt.show()
+    print(xy_kp1)
+    print(xy_kp2)
+    
